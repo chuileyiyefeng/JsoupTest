adb782abe6c99449d48e0b011de2dc35057af970
chuileyiyefeng <panyi123_ok@163.com>
2017-03-10 17:38:06 +0800

    5：50更新

--- app/src/main/java/my/com/jsouptest/MainActivity.java
+++ app/src/main/java/my/com/jsouptest/MainActivity.java
@@ -2,12 +2,43 @@ package my.com.jsouptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Document doc = Jsoup.connect("http://www.qiushibaike.com/8hr/page/1/").get();
+                    Elements els = doc.select("a.contentHref");
+                    for (int i = 0; i < els.size(); i++) {
+                        Element el = els.get(i);
+                        String href = el.attr("href");
+                        Document doc_det = Jsoup.connect("http://www.qiushibaike.com/" + href).get();
+                        Elements els_det = doc_det.select(".content");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
     }
 }
